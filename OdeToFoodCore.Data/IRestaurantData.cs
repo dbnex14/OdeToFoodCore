8016fc6dc1c7922835bfb4ec4004c9f589805211
Dino Buljubasic <dbuljubasic@squirrelsystems.com>
2019-09-28 13:17:39 -0700

    Added logic to display list of restaurants from in memory mock data.

---
+++ OdeToFoodCore.Data/IRestaurantData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OdeToFoodCore.Core;
+
+namespace OdeToFoodCore.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
--- OdeToFoodCore/Pages/Restaurants/List.cshtml.cs
+++ OdeToFoodCore/Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFoodCore.Core;
+using OdeToFoodCore.Data;
 
 namespace OdeToFoodCore.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
         public void OnGet()
         {
+            Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
